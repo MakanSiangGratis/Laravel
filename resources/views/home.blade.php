ea69c1f79a923e8810f4cce5e4b00e392aea0950
MakanSiangGratis <maulanairsyad014@gmail.com>
2024-08-13 08:33:02 +0700

    JAWA

---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ asset('css/desain.css') }}">
+    <title>Sulawesi</title>
+</head>
+<body>
+    <h1>Sulawesi</h1>
+    <ul>
+        <li>Sejarah</li>
+        <li>Geografis</li>
+        <li>Tempat Wisata</li>
+    </ul>
+
+    <h2>Sejarah</h2>
+    <img src="{{asset('gambar/indonesia - sulawesi.jpeg')}}" alt="Sejarah" width="500" height="300"
+    <p><br>Pulau Sulawesi ditemukan pertama kali oleh <em>Russe Wallace</em>. adalah seorang berkebangsaan Inggris yang melakukan perjalanan mengelilingi Indonesia yang dimulai dari
+        pulau Borneo sampai ke pulau Irian, termasuk ke pulau Sulawesi pada tahun 1856 sampai tahun 1862. Perjalanan panjang Wallace di Sulawesi, dimulai dari Ujung Pandang (Makassar) pada bulan September-Desember tahun 1856. Wallace kemudian melanjutkan perjalanan ke Manado dan Minahasa serta pulau-pulau kecil disekitarnya pada tahun 1859.
+        <br>Setelah melakukan perjalanan, Wallace menyatakan bawa posisi Sulawesi akan mudah menerima imigran dari semua sisi dibandingkan dengan pulau Jawa. Hal ini disebabkan oleh letak 
+        Sulawesi yang berada di tengah-tengah kepulauan yang sebelah utara berbatasan dengan Filipina, sebelah barat dengan Borneo, sebelah timur dengan pulau Maluku dan sebelah selatan dengan kelompok pulau Timor.</p>
+    
+    <h2>Geografis</h2>
+    <img src="{{asset('gambar/Sulawesi Island, Indonesia.jpeg')}}" alt="Geografis" width="500" height="300"
+    <p><br>Sulawesi adalah pulau terbesar kesebelas di dunia dan meliputi area seluas 188.563,19 km2. Bagian tengah pulau ini bergunung-gunung dengan permukaan kasar sehingga semenanjung di Sulawesi 
+        pada dasarnya jauh satu sama lain dan lebih mudah dijangkau melalui laut daripada melalui jalan darat.</p>
+
+    <h2>Tempat Wisata</h2>
+    <p>Tempat Wisata di Sulawesi meliputi:</p>
+    <ul>
+        <li><button><a href="https://www.google.com/search?sca_esv=37cb248e0107facb&sca_upv=1&rlz=1C1GCEA_enID1093ID1096&sxsrf=ADLYWIJlnp2fslfGCuiw6Fx9YPX4MwQn4g:1718194261190&q=Balai+Taman+Nasional+Bantimurung+Bulusaraung&stick=H4sIAAAAAAAAAONgFuLQz9U3MDcuyFbiArGSzM1ycs20FLOTrfRz8pMTSzLz8-AMq8SSkqLEZBCzeBGrjlNiTmKmQkhibmKegl9iMVA0MUfBKTGvJDO3tKg0L13BqTSntDixKBHIBgCLB2rPagAAAA&sa=X&ved=2ahUKEwjXo9-0hNaGAxWUSmwGHdwnCoQQ2coHegQIFxAD&cshid=1718194326844901&biw=1366&bih=633&dpr=1">Balai Taman Nasional Bantimurung Bulusaraung</a></button></li>
+        <li><button><a href="https://www.google.com/search?sca_esv=37cb248e0107facb&sca_upv=1&rlz=1C1GCEA_enID1093ID1096&sxsrf=ADLYWIJlnp2fslfGCuiw6Fx9YPX4MwQn4g:1718194261190&q=Kampoeng+Karst+Rammang+Rammang&stick=H4sIAAAAAAAAAONgFuLQz9U3MDcuyFbi1U_XNzRMMitKzs61zNZSzE620s_JT04syczPgzOsEktKihKTQcziRaxy3om5BfmpeekK3olFxSUKQYm5uYlAHpQGALDNKEJfAAAA&sa=X&ved=2ahUKEwjXo9-0hNaGAxWUSmwGHdwnCoQQ2coHegQIGBAD&cshid=1718194326844901&biw=1366&bih=633&dpr=1">Kampoeng Karst Rammang Rammang</a></button></li>
+        <li><button><a href="https://www.google.com/search?sca_esv=37cb248e0107facb&sca_upv=1&rlz=1C1GCEA_enID1093ID1096&sxsrf=ADLYWIJlnp2fslfGCuiw6Fx9YPX4MwQn4g:1718194261190&q=Benteng+Rotterdam&stick=H4sIAAAAAAAAAONgFuLQz9U3MDcuyFbiArHSCrLSqky1FLOTrfRz8pMTSzLz8-AMq8SSkqLEZBCzeBGroFNqXklqXrpCUH5JSWpRSmIuAEofCMxPAAAA&sa=X&ved=2ahUKEwjXo9-0hNaGAxWUSmwGHdwnCoQQ2coHegQIGRAD&cshid=1718194326844901&biw=1366&bih=633&dpr=1">Benteng Rotterdam</a></button></li>
+        <li><button><a href="https://www.google.com/search?sca_esv=37cb248e0107facb&sca_upv=1&rlz=1C1GCEA_enID1093ID1096&sxsrf=ADLYWIJlnp2fslfGCuiw6Fx9YPX4MwQn4g:1718194261190&q=Air+Terjun+Saluopa&stick=H4sIAAAAAAAAAONgFuLQz9U3MDcuyFbi1U_XNzRMNqxIMTIvStJSzE620s_JT04syczPgzOsEktKihKTQcziRaxCjplFCiGpRVmleQrBiTml-QWJAJpH9dBTAAAA&sa=X&ved=2ahUKEwjXo9-0hNaGAxWUSmwGHdwnCoQQ2coHegQIFhAD&cshid=1718194326844901&biw=1366&bih=633&dpr=1">Air Terjun Saluopa</a></button></li>
+        <li><button><a href="https://www.google.com/search?sca_esv=37cb248e0107facb&sca_upv=1&rlz=1C1GCEA_enID1093ID1096&sxsrf=ADLYWIJlnp2fslfGCuiw6Fx9YPX4MwQn4g:1718194261190&q=Pantai+Losari&stick=H4sIAAAAAAAAAONgFuLQz9U3MDcuyFbi1k_XNzQyzC40sajQUsxOttLPyU9OLMnMz4MzrBJLSooSk0HM4kWsvAGJeSWJmQo--cWJRZkAYIxC0UwAAAA&sa=X&ved=2ahUKEwjXo9-0hNaGAxWUSmwGHdwnCoQQ2coHegQIfxAD&biw=1366&bih=633&dpr=1">Pantai Losari</a></button></li>
+        <li><button><a href="https://www.google.com/search?q=tempat+wisata+sulawesi&rlz=1C1GCEA_enID1093ID1096&oq=t&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIOCAEQRRgnGDsYgAQYigUyBggCEEUYOTIPCAMQRRg7GIMBGLEDGIAEMg0IBBAAGIMBGLEDGIAEMg0IBRAAGIMBGLEDGIAEMhMIBhAuGIMBGMcBGLEDGNEDGIAEMg0IBxAuGMcBGNEDGIAEMhgICBAuGEMYgwEYxwEYsQMY0QMYgAQYigUyDQgJEAAYgwEYsQMYgATSAQk0MDE3ajBqMTWoAgmwAgE&sourceid=chrome&ie=UTF-8">Dan masih banyak lagi!</a></button></li>
+    </ul>
+
+
+</body>
+<footer>
+    <a href="halaman2.html"> Klik ke Halaman 2</a>
+    
+</footer>
+</html>
